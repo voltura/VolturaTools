ff20a0a48f050dc5ccd245be4b26b1b48563949b
Joakim Skoglund <joakim@voltura.se>
2018-09-27 22:49:13 +0200

    Less code, no form

--- WeekNumber/Program.cs
+++ WeekNumber/Program.cs
@@ -1,11 +1,36 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using System.Drawing;
+using System.Globalization;
 
-namespace WeekNumber
+internal class Program
 {
-    internal static class Program
+    NotifyIcon ni = new NotifyIcon() { Visible = true };
+    Timer t;
+
+    static void Main()
     {
-        [STAThread]
-        static void Main() => Application.Run(new MainForm());
+        Program p = new Program();
+        Application.Run();
+    }
+
+    public Program()
+    {
+        ni.Icon = GetTextIcon(ThisWeek);
+        t = new Timer() { Interval = 60000, Enabled = true };
+        t.Tick += delegate(object o, System.EventArgs e) {
+            if (ni.Text != ThisWeek) ni.Icon = GetTextIcon(ThisWeek);
+        };
+    }
+
+    static string ThisWeek => CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(
+            System.DateTime.Now, CalendarWeekRule.FirstFourDayWeek,
+            System.DayOfWeek.Monday).ToString();
+
+    static Icon GetTextIcon(string t)
+    {
+        Bitmap bitmap = new Bitmap(48, 48);
+        Graphics.FromImage(bitmap).DrawString(t, 
+            new Font(FontFamily.GenericMonospace, 26f, FontStyle.Bold) { }, Brushes.White, 0f, 0f);
+        return Icon.FromHandle(bitmap.GetHicon());
     }
 }
