1a5bd668d701acd26cbe24f0e445181de4c70a2a
Joakim Skoglund <joakim@voltura.se>
2018-10-09 02:00:09 +0200

    Code refactoring to increase maintainability

--- WeekNumber/TaskbarGui.cs
+++ WeekNumber/TaskbarGui.cs
@@ -76,32 +76,49 @@ namespace WeekNumber
             using (var bitmap = new Bitmap(64, 64))
             using (var graphics = Graphics.FromImage(bitmap))
             {
-                graphics.FillRectangle(Brushes.Black, 2, 2, 62, 62);
-                using (var whitePen = new Pen(Color.White, 4f))
-                {
-                    graphics.DrawRectangle(whitePen, 2, 2, 60, 60);
-                }
-                graphics.FillRectangle(Brushes.White, 10, 2, 6, 12);
-                graphics.FillRectangle(Brushes.White, 48, 2, 6, 12);
-                using (var font = new Font(FontFamily.GenericMonospace, 12f, FontStyle.Bold))
-                {
-                    graphics.DrawString(weekNumber.ToString().PadLeft(2, '0').Substring(0, 2), font, Brushes.White, -6f, 10f);
-                }
-                var bHicon = bitmap.GetHicon();
-                var prevIcon = notifyIcon.Icon;
-                var newIcon = Icon.FromHandle(bHicon);
-                notifyIcon.Icon = new Icon(newIcon, SystemInformation.SmallIconSize);
-                if (prevIcon != null)
-                {
-                    NativeMethods.DestroyIcon(prevIcon.Handle);
-                    prevIcon.Dispose();
-                }
-                if (newIcon != null)
-                {
-                    NativeMethods.DestroyIcon(newIcon.Handle);
-                    newIcon.Dispose();
-                }
+                DrawBackgroundOnGraphics(graphics);
+                DrawWeekNumberOnGraphics(weekNumber, graphics);
+                SetIconFromBitmapToNotifyIcon(notifyIcon, bitmap);
             }
+        }
+
+        private static void SetIconFromBitmapToNotifyIcon(NotifyIcon notifyIcon, Bitmap bitmap)
+        {
+            var bHicon = bitmap.GetHicon();
+            var prevIcon = notifyIcon.Icon;
+            var newIcon = Icon.FromHandle(bHicon);
+            notifyIcon.Icon = new Icon(newIcon, SystemInformation.SmallIconSize);
+            CleanupIcon(ref prevIcon);
+            CleanupIcon(ref newIcon);
+        }
+
+        private static void DrawBackgroundOnGraphics(Graphics graphics)
+        {
+            graphics.FillRectangle(Brushes.Black, 2, 2, 62, 62);
+            using (var whitePen = new Pen(Color.White, 4f))
+            {
+                graphics.DrawRectangle(whitePen, 2, 2, 60, 60);
+            }
+            graphics.FillRectangle(Brushes.White, 10, 2, 6, 12);
+            graphics.FillRectangle(Brushes.White, 48, 2, 6, 12);
+        }
+
+        private static void DrawWeekNumberOnGraphics(int weekNumber, Graphics graphics)
+        {
+            using (var font = new Font(FontFamily.GenericMonospace, 12f, FontStyle.Bold))
+            {
+                graphics.DrawString(weekNumber.ToString().PadLeft(2, '0').Substring(0, 2), font, Brushes.White, -6f, 10f);
+            }
+        }
+
+        private static void CleanupIcon(ref Icon icon)
+        {
+            if (icon is null)
+            {
+                return;
+            }
+            NativeMethods.DestroyIcon(icon.Handle);
+            icon.Dispose();
         }
 
         #endregion Private static UpdateIcon method
@@ -141,21 +158,31 @@ namespace WeekNumber
             {
                 return;
             }
+            CleanupNotifyIcon();
+            CleanupContextMenu();
+        }
+
+        private void CleanupContextMenu()
+        {
+            _contextMenu?.Dispose();
+            _contextMenu = null;
+        }
+
+        private void CleanupNotifyIcon()
+        {
             if (_notifyIcon != null)
             {
                 _notifyIcon.Visible = false;
-                if (_notifyIcon?.Icon != null)
+                if (_notifyIcon.Icon != null)
                 {
                     NativeMethods.DestroyIcon(_notifyIcon.Icon.Handle);
-                    _notifyIcon?.Icon?.Dispose();
+                    _notifyIcon.Icon?.Dispose();
                 }
                 _notifyIcon.ContextMenu?.MenuItems.Clear();
                 _notifyIcon.ContextMenu?.Dispose();
                 _notifyIcon.Dispose();
                 _notifyIcon = null;
             }
-            _contextMenu?.Dispose();
-            _contextMenu = null;
         }
 
         #endregion IDisposable methods
