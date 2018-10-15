cd498f7bd382329c7adcc7dcf4da2f6a6059dccb
Joakim Skoglund <joakim@voltura.se>
2018-10-16 00:30:33 +0200

    Added comments to public interface

--- WeekNumber/IGui.cs
+++ WeekNumber/IGui.cs
@@ -1,8 +1,18 @@
 ﻿namespace WeekNumber
 {
+    /// <summary>
+    /// GUI Interface
+    /// </summary>
     public interface IGui
     {
+        /// <summary>
+        /// Updates icon on GUI with given week number
+        /// </summary>
+        /// <param name="weekNumber"></param>
         void UpdateIcon(int weekNumber);
+        /// <summary>
+        /// Disposes GUI
+        /// </summary>
         void Dispose();
     }
 }
