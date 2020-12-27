1f50056dde0169c144d478adfca57a27451951cc
Joakim Skoglund <joakim@voltura.se>
2020-12-27 08:20:36 +0100

    Tinkered with memory issue

--- ProxyToogle/ProxyToogle.cs
+++ ProxyToogle/ProxyToogle.cs
@@ -31,43 +31,79 @@ namespace ProxyToogle
         private readonly Icon _onIcon, _offIcon;
         private readonly Timer _timer;
         private bool _proxyEnabled;
-        private string _proxyServer;
+        //private string _proxyServer;
         private const string REGKEY = @"Software\Microsoft\Windows\CurrentVersion\Internet Settings";
+        //private readonly string PROXY_ON = "Proxy is ON";
+        //private readonly string PROXY_OFF = "Proxy is OFF";
 
         internal AppContext()
         {
             _onIcon = CreateIcon(Color.MediumSpringGreen, Color.Black);
             _offIcon = CreateIcon(Color.Crimson, Color.White);
-            _proxyEnabled = ProxyEnabled();
-            _proxyServer = ProxyServer();
-            _notifyIcon = new NotifyIcon() { Visible = true, Icon = _proxyEnabled ? _onIcon : _offIcon };
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(REGKEY, true))
+            {
+                _proxyEnabled = (int)key.GetValue("ProxyEnable", 0) == 1;
+          //      _proxyServer = (string)key.GetValue("ProxyServer", "No proxy server defined");
+            }
+            //PROXY_ON += "\r\nProxy: " + _proxyServer;
+            //PROXY_OFF += "\r\nProxy: " + _proxyServer;
+
+            Size SIZE = new Size(256, 256);
+            Icon iconCopy = new Icon(_proxyEnabled ? _onIcon : _offIcon, SIZE);
+            _notifyIcon = new NotifyIcon() { Visible = true, Icon = iconCopy };
             _notifyIcon.Click += (s, e) => { if (((MouseEventArgs)e).Button == MouseButtons.Left) {
-                    Registry.CurrentUser.OpenSubKey(REGKEY, true).SetValue("ProxyEnable", ProxyEnabled() ? 0 : 1);
+                    using (RegistryKey key = Registry.CurrentUser.OpenSubKey(REGKEY, true))
+                    {
+                        _proxyEnabled = (int)key.GetValue("ProxyEnable", 0) == 1;
+                        key.SetValue("ProxyEnable", _proxyEnabled ? 0 : 1);
+                    }
                     NativeMethods.InternetSetOption(IntPtr.Zero, NativeMethods.INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
                     NativeMethods.InternetSetOption(IntPtr.Zero, NativeMethods.INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
                 }
             };
-            _notifyIcon.ContextMenu = new ContextMenu(new MenuItem[1] { new MenuItem(Properties.Resources.ExitProxyToogle, (o, e) => Application.Exit() ) });
+            _notifyIcon.ContextMenu = new ContextMenu(new MenuItem[1] { new MenuItem(Properties.Resources.ExitProxyToogle, (o, e) => {
+                _notifyIcon.Visible = false;
+                if (_notifyIcon.Icon != null)
+                {
+                    NativeMethods.DestroyIcon(_notifyIcon.Icon.Handle);
+                    if (iconCopy != null)
+                    {
+                        NativeMethods.DestroyIcon(iconCopy.Handle);
+                        iconCopy?.Dispose();
+                    }
+                    _notifyIcon.Icon?.Dispose();
+                    GC.WaitForPendingFinalizers();
+                    GC.Collect();
+                }
+                Application.Exit(); 
+            } ) });
             _timer = new Timer { Interval = 1000, Enabled = true };
             _timer.Tick += (s, e) => {
                 _timer?.Stop();
                 Application.DoEvents();
-                _proxyEnabled = ProxyEnabled();
-                _proxyServer = ProxyServer();
-                _notifyIcon.Icon = _proxyEnabled ? _onIcon : _offIcon;
-                _notifyIcon.Text = $"Proxy is {(_proxyEnabled ? "ON" : "OFF")}\r\nProxy: {_proxyServer}";
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(REGKEY, true))
+                {
+                    _proxyEnabled = (int)key.GetValue("ProxyEnable", 0) == 1;
+            //        _proxyServer = (string)key.GetValue("ProxyServer", "No proxy server defined");
+                }
+                if (_notifyIcon.Icon != null)
+                {
+                    NativeMethods.DestroyIcon(_notifyIcon.Icon.Handle);
+                    if (iconCopy != null)
+                    {
+                        NativeMethods.DestroyIcon(iconCopy.Handle);
+                        iconCopy?.Dispose();
+                    }
+                    _notifyIcon.Icon?.Dispose();
+                    GC.WaitForPendingFinalizers();
+                    GC.Collect();
+                }
+                iconCopy = new Icon(_proxyEnabled ? _onIcon : _offIcon, SIZE);
+                _notifyIcon.Icon = iconCopy;
+                // MEMORY LEAK ON ROW BELOW! How to handle?
+                //_notifyIcon.Text = _proxyEnabled ? PROXY_ON : PROXY_OFF;
                 _timer?.Start();
             };
-        }
-
-        internal static bool ProxyEnabled()
-        {
-            return (int)Registry.CurrentUser.OpenSubKey(REGKEY, true).GetValue("ProxyEnable", 0) == 1;
-        }
-
-        internal static string ProxyServer()
-        {
-            return (string)Registry.CurrentUser.OpenSubKey(REGKEY, true).GetValue("ProxyServer", "No proxy server defined");
         }
 
         private static Icon CreateIcon(Color backColor, Color textColor)
@@ -89,9 +125,14 @@ namespace ProxyToogle
 
     internal static class NativeMethods
     {
-        [System.Runtime.InteropServices.DllImport("wininet.dll")]
-        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
         public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
         public const int INTERNET_OPTION_REFRESH = 37;
+
+        [System.Runtime.InteropServices.DllImport("wininet.dll")]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        internal static extern bool DestroyIcon(IntPtr handle);
     }
 }
