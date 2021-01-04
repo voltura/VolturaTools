7b8ab223a7d0ac1994e144e498d9b9e5380b0d33
Joakim Skoglund <joakim@voltura.se>
2021-01-04 06:37:35 +0100

    Fixed UI control alignment issue on form

--- DiskSpace/Forms/EmailSettingsForm.Designer.cs
+++ DiskSpace/Forms/EmailSettingsForm.Designer.cs
@@ -71,25 +71,25 @@ namespace DiskSpace.Forms
             this.settingsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.settingsPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
+            this.settingsPanel.Controls.Add(this.txtSmtpPort);
+            this.settingsPanel.Controls.Add(this.txtSmtpServer);
             this.settingsPanel.Controls.Add(this.chkUseSsl);
             this.settingsPanel.Controls.Add(this.txtEmailPassword);
             this.settingsPanel.Controls.Add(this.lblPassword);
             this.settingsPanel.Controls.Add(this.lblUser);
             this.settingsPanel.Controls.Add(this.txtEmailUserName);
             this.settingsPanel.Controls.Add(this.lblPort);
-            this.settingsPanel.Controls.Add(this.txtSmtpPort);
             this.settingsPanel.Controls.Add(this.lblSmtpServer);
-            this.settingsPanel.Controls.Add(this.txtSmtpServer);
             this.settingsPanel.Controls.Add(this.lblTo);
             this.settingsPanel.Controls.Add(this.txtToEmail);
             this.settingsPanel.Controls.Add(this.lblFromEmail);
             this.settingsPanel.Controls.Add(this.txtFromEmail);
             this.settingsPanel.Controls.Add(this.btnSendTestEmail);
             this.settingsPanel.Controls.Add(this.btnSave);
-            this.settingsPanel.Location = new System.Drawing.Point(0, 32);
+            this.settingsPanel.Location = new System.Drawing.Point(0, 40);
             this.settingsPanel.Margin = new System.Windows.Forms.Padding(0);
             this.settingsPanel.Name = "settingsPanel";
-            this.settingsPanel.Size = new System.Drawing.Size(548, 237);
+            this.settingsPanel.Size = new System.Drawing.Size(616, 296);
             this.settingsPanel.TabIndex = 2;
             // 
             // chkUseSsl
@@ -107,10 +107,10 @@ namespace DiskSpace.Forms
             this.chkUseSsl.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue;
             this.chkUseSsl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.chkUseSsl.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkUseSsl.Location = new System.Drawing.Point(12, 193);
+            this.chkUseSsl.Location = new System.Drawing.Point(14, 241);
             this.chkUseSsl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chkUseSsl.Name = "chkUseSsl";
-            this.chkUseSsl.Size = new System.Drawing.Size(87, 27);
+            this.chkUseSsl.Size = new System.Drawing.Size(107, 34);
             this.chkUseSsl.TabIndex = 21;
             this.chkUseSsl.Text = "Use SSL";
             this.chkUseSsl.UseVisualStyleBackColor = false;
@@ -122,10 +122,11 @@ namespace DiskSpace.Forms
             this.txtEmailPassword.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::DiskSpace.Properties.Settings.Default, "emailPassword", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.txtEmailPassword.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
             this.txtEmailPassword.ForeColor = System.Drawing.Color.White;
-            this.txtEmailPassword.Location = new System.Drawing.Point(435, 151);
+            this.txtEmailPassword.Location = new System.Drawing.Point(489, 189);
+            this.txtEmailPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtEmailPassword.Name = "txtEmailPassword";
             this.txtEmailPassword.PasswordChar = '•';
-            this.txtEmailPassword.Size = new System.Drawing.Size(101, 30);
+            this.txtEmailPassword.Size = new System.Drawing.Size(113, 35);
             this.txtEmailPassword.TabIndex = 20;
             this.txtEmailPassword.Text = global::DiskSpace.Properties.Settings.Default.emailPassword;
             // 
@@ -133,9 +134,9 @@ namespace DiskSpace.Forms
             // 
             this.lblPassword.AutoSize = true;
             this.lblPassword.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
-            this.lblPassword.Location = new System.Drawing.Point(343, 153);
+            this.lblPassword.Location = new System.Drawing.Point(386, 191);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(82, 23);
+            this.lblPassword.Size = new System.Drawing.Size(103, 30);
             this.lblPassword.TabIndex = 19;
             this.lblPassword.Text = "Password";
             // 
@@ -143,9 +144,9 @@ namespace DiskSpace.Forms
             // 
             this.lblUser.AutoSize = true;
             this.lblUser.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
-            this.lblUser.Location = new System.Drawing.Point(12, 153);
+            this.lblUser.Location = new System.Drawing.Point(14, 191);
             this.lblUser.Name = "lblUser";
-            this.lblUser.Size = new System.Drawing.Size(44, 23);
+            this.lblUser.Size = new System.Drawing.Size(56, 30);
             this.lblUser.TabIndex = 18;
             this.lblUser.Text = "User";
             // 
@@ -156,9 +157,10 @@ namespace DiskSpace.Forms
             this.txtEmailUserName.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::DiskSpace.Properties.Settings.Default, "emailUserName", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.txtEmailUserName.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
             this.txtEmailUserName.ForeColor = System.Drawing.Color.White;
-            this.txtEmailUserName.Location = new System.Drawing.Point(73, 151);
+            this.txtEmailUserName.Location = new System.Drawing.Point(94, 189);
+            this.txtEmailUserName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtEmailUserName.Name = "txtEmailUserName";
-            this.txtEmailUserName.Size = new System.Drawing.Size(264, 30);
+            this.txtEmailUserName.Size = new System.Drawing.Size(285, 35);
             this.txtEmailUserName.TabIndex = 17;
             this.txtEmailUserName.Text = global::DiskSpace.Properties.Settings.Default.emailUserName;
             // 
@@ -166,9 +168,9 @@ namespace DiskSpace.Forms
             // 
             this.lblPort.AutoSize = true;
             this.lblPort.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
-            this.lblPort.Location = new System.Drawing.Point(431, 107);
+            this.lblPort.Location = new System.Drawing.Point(485, 133);
             this.lblPort.Name = "lblPort";
-            this.lblPort.Size = new System.Drawing.Size(41, 23);
+            this.lblPort.Size = new System.Drawing.Size(54, 30);
             this.lblPort.TabIndex = 16;
             this.lblPort.Text = "Port";
             // 
@@ -179,9 +181,10 @@ namespace DiskSpace.Forms
             this.txtSmtpPort.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::DiskSpace.Properties.Settings.Default, "smtpPort", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.txtSmtpPort.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
             this.txtSmtpPort.ForeColor = System.Drawing.Color.White;
-            this.txtSmtpPort.Location = new System.Drawing.Point(478, 105);
+            this.txtSmtpPort.Location = new System.Drawing.Point(545, 131);
+            this.txtSmtpPort.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtSmtpPort.Name = "txtSmtpPort";
-            this.txtSmtpPort.Size = new System.Drawing.Size(58, 30);
+            this.txtSmtpPort.Size = new System.Drawing.Size(58, 35);
             this.txtSmtpPort.TabIndex = 15;
             this.txtSmtpPort.Text = global::DiskSpace.Properties.Settings.Default.smtpPort;
             this.txtSmtpPort.TextChanged += new System.EventHandler(this.SmtpPort_TextChanged);
@@ -191,9 +194,9 @@ namespace DiskSpace.Forms
             // 
             this.lblSmtpServer.AutoSize = true;
             this.lblSmtpServer.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
-            this.lblSmtpServer.Location = new System.Drawing.Point(12, 107);
+            this.lblSmtpServer.Location = new System.Drawing.Point(14, 134);
             this.lblSmtpServer.Name = "lblSmtpServer";
-            this.lblSmtpServer.Size = new System.Drawing.Size(59, 23);
+            this.lblSmtpServer.Size = new System.Drawing.Size(74, 30);
             this.lblSmtpServer.TabIndex = 14;
             this.lblSmtpServer.Text = "Server";
             // 
@@ -204,9 +207,10 @@ namespace DiskSpace.Forms
             this.txtSmtpServer.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::DiskSpace.Properties.Settings.Default, "smtpServer", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.txtSmtpServer.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
             this.txtSmtpServer.ForeColor = System.Drawing.Color.White;
-            this.txtSmtpServer.Location = new System.Drawing.Point(73, 105);
+            this.txtSmtpServer.Location = new System.Drawing.Point(94, 132);
+            this.txtSmtpServer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtSmtpServer.Name = "txtSmtpServer";
-            this.txtSmtpServer.Size = new System.Drawing.Size(352, 30);
+            this.txtSmtpServer.Size = new System.Drawing.Size(384, 35);
             this.txtSmtpServer.TabIndex = 13;
             this.txtSmtpServer.Text = global::DiskSpace.Properties.Settings.Default.smtpServer;
             // 
@@ -214,9 +218,9 @@ namespace DiskSpace.Forms
             // 
             this.lblTo.AutoSize = true;
             this.lblTo.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
-            this.lblTo.Location = new System.Drawing.Point(12, 58);
+            this.lblTo.Location = new System.Drawing.Point(14, 72);
             this.lblTo.Name = "lblTo";
-            this.lblTo.Size = new System.Drawing.Size(27, 23);
+            this.lblTo.Size = new System.Drawing.Size(36, 30);
             this.lblTo.TabIndex = 12;
             this.lblTo.Text = "To";
             // 
@@ -227,9 +231,10 @@ namespace DiskSpace.Forms
             this.txtToEmail.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::DiskSpace.Properties.Settings.Default, "ToEmailAddress", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.txtToEmail.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
             this.txtToEmail.ForeColor = System.Drawing.Color.White;
-            this.txtToEmail.Location = new System.Drawing.Point(73, 56);
+            this.txtToEmail.Location = new System.Drawing.Point(94, 70);
+            this.txtToEmail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtToEmail.Name = "txtToEmail";
-            this.txtToEmail.Size = new System.Drawing.Size(463, 30);
+            this.txtToEmail.Size = new System.Drawing.Size(509, 35);
             this.txtToEmail.TabIndex = 11;
             this.txtToEmail.Text = global::DiskSpace.Properties.Settings.Default.ToEmailAddress;
             // 
@@ -237,9 +242,9 @@ namespace DiskSpace.Forms
             // 
             this.lblFromEmail.AutoSize = true;
             this.lblFromEmail.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
-            this.lblFromEmail.Location = new System.Drawing.Point(12, 16);
+            this.lblFromEmail.Location = new System.Drawing.Point(14, 20);
             this.lblFromEmail.Name = "lblFromEmail";
-            this.lblFromEmail.Size = new System.Drawing.Size(50, 23);
+            this.lblFromEmail.Size = new System.Drawing.Size(64, 30);
             this.lblFromEmail.TabIndex = 10;
             this.lblFromEmail.Text = "From";
             // 
@@ -250,9 +255,10 @@ namespace DiskSpace.Forms
             this.txtFromEmail.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::DiskSpace.Properties.Settings.Default, "FromEmailAddress", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.txtFromEmail.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
             this.txtFromEmail.ForeColor = System.Drawing.Color.White;
-            this.txtFromEmail.Location = new System.Drawing.Point(73, 14);
+            this.txtFromEmail.Location = new System.Drawing.Point(94, 18);
+            this.txtFromEmail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtFromEmail.Name = "txtFromEmail";
-            this.txtFromEmail.Size = new System.Drawing.Size(463, 30);
+            this.txtFromEmail.Size = new System.Drawing.Size(509, 35);
             this.txtFromEmail.TabIndex = 9;
             this.txtFromEmail.Text = global::DiskSpace.Properties.Settings.Default.FromEmailAddress;
             // 
@@ -264,10 +270,10 @@ namespace DiskSpace.Forms
             this.btnSendTestEmail.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue;
             this.btnSendTestEmail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSendTestEmail.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSendTestEmail.Location = new System.Drawing.Point(219, 186);
+            this.btnSendTestEmail.Location = new System.Drawing.Point(246, 232);
             this.btnSendTestEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSendTestEmail.Name = "btnSendTestEmail";
-            this.btnSendTestEmail.Size = new System.Drawing.Size(179, 40);
+            this.btnSendTestEmail.Size = new System.Drawing.Size(201, 50);
             this.btnSendTestEmail.TabIndex = 8;
             this.btnSendTestEmail.Text = "Send test email";
             this.btnSendTestEmail.UseVisualStyleBackColor = false;
@@ -281,10 +287,10 @@ namespace DiskSpace.Forms
             this.btnSave.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightBlue;
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSave.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(404, 186);
+            this.btnSave.Location = new System.Drawing.Point(454, 232);
             this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(132, 40);
+            this.btnSave.Size = new System.Drawing.Size(148, 50);
             this.btnSave.TabIndex = 7;
             this.btnSave.Text = global::DiskSpace.Properties.Resources.SaveButtonTitle;
             this.btnSave.UseVisualStyleBackColor = false;
@@ -297,8 +303,8 @@ namespace DiskSpace.Forms
             this.lblSettingsTitle.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSettingsTitle.Location = new System.Drawing.Point(0, 0);
             this.lblSettingsTitle.Name = "lblSettingsTitle";
-            this.lblSettingsTitle.Padding = new System.Windows.Forms.Padding(32, 0, 0, 0);
-            this.lblSettingsTitle.Size = new System.Drawing.Size(548, 32);
+            this.lblSettingsTitle.Padding = new System.Windows.Forms.Padding(36, 0, 0, 0);
+            this.lblSettingsTitle.Size = new System.Drawing.Size(616, 40);
             this.lblSettingsTitle.TabIndex = 1;
             this.lblSettingsTitle.Text = "Email settings - SMTP";
             this.lblSettingsTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -308,10 +314,10 @@ namespace DiskSpace.Forms
             // minimizePanelFrame
             // 
             this.minimizePanelFrame.Controls.Add(this.minimizePanel);
-            this.minimizePanelFrame.Location = new System.Drawing.Point(510, 0);
+            this.minimizePanelFrame.Location = new System.Drawing.Point(574, 0);
             this.minimizePanelFrame.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.minimizePanelFrame.Name = "minimizePanelFrame";
-            this.minimizePanelFrame.Size = new System.Drawing.Size(36, 32);
+            this.minimizePanelFrame.Size = new System.Drawing.Size(40, 40);
             this.minimizePanelFrame.TabIndex = 3;
             this.minimizePanelFrame.Click += new System.EventHandler(this.MinimizePanelFrame_Click);
             this.minimizePanelFrame.MouseEnter += new System.EventHandler(this.MinimizePanel_MouseEnter);
@@ -321,10 +327,10 @@ namespace DiskSpace.Forms
             // 
             this.minimizePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.minimizePanel.BackColor = System.Drawing.Color.White;
-            this.minimizePanel.Location = new System.Drawing.Point(7, 7);
+            this.minimizePanel.Location = new System.Drawing.Point(8, 9);
             this.minimizePanel.Margin = new System.Windows.Forms.Padding(0);
             this.minimizePanel.Name = "minimizePanel";
-            this.minimizePanel.Size = new System.Drawing.Size(25, 8);
+            this.minimizePanel.Size = new System.Drawing.Size(28, 10);
             this.minimizePanel.TabIndex = 3;
             this.minimizePanel.Click += new System.EventHandler(this.MinimizePanel_Click);
             this.minimizePanel.MouseEnter += new System.EventHandler(this.MinimizePanel_MouseEnter);
@@ -337,17 +343,17 @@ namespace DiskSpace.Forms
             this.titleIcon.Location = new System.Drawing.Point(4, 0);
             this.titleIcon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.titleIcon.Name = "titleIcon";
-            this.titleIcon.Size = new System.Drawing.Size(32, 32);
+            this.titleIcon.Size = new System.Drawing.Size(36, 40);
             this.titleIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.titleIcon.TabIndex = 4;
             this.titleIcon.TabStop = false;
             // 
             // EmailSettingsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.ClientSize = new System.Drawing.Size(548, 269);
+            this.ClientSize = new System.Drawing.Size(616, 336);
             this.ControlBox = false;
             this.Controls.Add(this.titleIcon);
             this.Controls.Add(this.minimizePanelFrame);
